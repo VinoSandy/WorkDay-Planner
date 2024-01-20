2e7db42764840ca6c110e35ea1b69b65d054aaa9
Vinosandy <rvinothini@yahoo.co.in>
2024-01-20 17:16:11 +0000

    script.js updated with code to set attribute present past and future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,47 @@
-var todayDateTime = dayjs().format("DDMMMMYYYY hh:mm:ss");
+/*var todayDateTime = dayjs().format("DDMMMMYYYY hh:mm:ss");
 
+//Displays current date and time in the browser
 $('#currentDay').text(todayDateTime);
+
+var currentHour = dayjs.format("hh");*/
+
+
+
+var todayDateTime= dayjs();
+
+var formattedDateTime = todayDateTime.format("DDMMMMYYYY hh:mm:ss");
+
+// Display the formatted date and time in the browser
+$('#currentDay').text(formattedDateTime);
+
+// Get the current hour using the formattedDateTime
+var currentHour = todayDateTime.format("hh");
+
+
+$(".hour-div").each(function(){
+
+var appHour= $(this).attr("id")
+
+console.log("id:"+appHour);
+
+if(currentHour == appHour){
+
+ $(this).addClass("present");
+
+console.log('current' +currentHour+ 'appHour' +appHour)
+console.log(currentHour===appHour);
+}
+
+if(currentHour > appHour){
+$(this).removeClass("present");
+$(this).addClass("past")
+}
+
+if(currentHour < appHour){
+$(this).removeClass("past");
+$(this).addClass("future");
+}
+
+
+
+})
